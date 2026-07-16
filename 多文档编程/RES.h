2d7aa4f46db05a6f5a2b4885efa37b5ea6bfd861
WQ9527FDT <Wq20031127@>
2026-07-16 09:24:17 +0800

    利用多文档编程来和指针传参文件保存实现银行基本系统

---
+++ 多文档编程/RES.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef  _RES_H_
+#define _RES_H_
+#define TEXTNAME "WQ,BANK"
+#endif 
+
+
+
+
+
---
+++ 多文档编程/myfile.h
@@ -0,0 +1,11 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include "wenjian.h"
+void menu(double* money);
+void show(double* money);
+void cun(double* money);
+void qu(double* money);
+void exit_program();

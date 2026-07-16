1c62ede59ab8ebb4a4918884e792aa0c00cee1d3
WQ9527FDT <Wq20031127@>
2026-07-16 21:45:05 +0800

    分析系统是大小端字节存储

---
+++ 利用memcpy/test.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <string.h>
+int main()
+{
+/*	int i = 1;
+	char a[10] = { 0 };
+	char* b= memcpy( a, &i, 1);
+	printf("%s\n", a[0] ? "小端" : "大端");
+*/ // 更简洁方法：
+	int i = 1;
+	printf("%s\n", *(char*)&i ? "小端" : "大端");
+	return 0;
+
+
+}

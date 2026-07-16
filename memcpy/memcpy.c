3008dc7db64f85ef5665691b2282b69fdc752514
WQ9527FDT <Wq20031127@>
2026-07-16 09:59:16 +0800

    简单使用memcpy内存拷贝函数

---
+++ memcpy/memcpy.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+int main()
+{
+	int  buff1[] = { 1,2,3,4,5,6,7,8,9 };
+	int  buff2[20] = { 0 };
+	memcpy(buff2, buff1, 20);  //利用字节大小  int 整型 一个 4 字节
+	rsize_t r = sizeof(buff2) / sizeof(buff2[0]);
+	for (int i = 0; i < r; i++)
+	{
+		printf("%d\n", buff2[i]);
+	}
+
+
+}

41cbd96e44ea7dba511f549ce3b8ad28ecc1a89d
WQ9527FDT <Wq20031127@>
2026-07-17 20:12:14 +0800

    实现文件简单加密

---
+++ 文件加密器（桌面版）/resource.h
@@ -0,0 +1,17 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ Éú³ÉµÄ°üº¬ÎÄ¼þ¡£
+// ¹© ÎÄ¼þ¼ÓÃÜÆ÷£¨×ÀÃæ°æ£©.rc Ê¹ÓÃ
+//
+#define IDI_ICON1                       101
+#define IDI_ICON2                       102
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
